14d27813b183510473a15f512a72a1b97616f38b
komal-8 <komalmangal970@gmail.com>
2024-06-05 20:55:37 +0530

    install react js and tailwind css

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>MERN_BLOG_APP</h1>
+    </>
+  )
+}
+
+export default App
